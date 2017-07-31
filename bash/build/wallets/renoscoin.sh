74b7455a082115fdc27e7210343a9f162ba04bff
Brad Cooper <fleshwounded.github@gmail.com>
2017-07-31 10:52:33 -0600

    Updated comments to include wallet tipping addresses
    Just in case, if anything is useful and your feeling generous please help maintain the infrastruture hosting these projects

--- bash/build/wallets/renoscoin.sh
+++ bash/build/wallets/renoscoin.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 #----------------------------------------------------------------------#
+#      author:  thebradfish
+#     twitter: 	@fleshwounded
+#     website: 	bradspodium.xyz
 #        name:  renoscoin.sh
 # description:  shell script compiles renos coin wallet daemon source
 #               and configures with random name and passwords, then it
@@ -8,11 +11,18 @@
 #               or run a master node you will need to configure it 
 #               before it will work the way you intend. otherwise use
 #               wallet as usual.
-#      author:  thebradfish
-#     twitter: 	@fleshwounded
-#     website: 	bradspodium.xyz
+#     funding:  if you find any of this useful, please consider tipping,
+#               and or donating, all funds are used to pay for virtual
+#				machines hosting various projects like this, thank you!
 #----------------------------------------------------------------------#
-
+#        excl:  EKQvE27DbGkTfEuvZ4wFpufdfp4M6gVMUZ
+#         rns:  RXtifjA6gnKanV4MLFsz7FBPWGEEFhab3s
+#		  chc:  Ca5yFQzYu3vEP9PCRBv6deqqqZFiqbEGXX
+#         ent:  EUd7C7uc8JUf3ibVrLRknusapsQpVBdMeB
+#         arc:  AXM3CwihR4Xoa63YiGBuuMiX8hkE43G7CT
+#         btc:  183mfMcQirWUQXD3wddzuYZPdd8ztKKjVZ
+#----------------------------------------------------------------------#
+						   
 git clone https://github.com/RenosCoin/RenosCoin.git  
 cd RenosCoin  
 cd src  
